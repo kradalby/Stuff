d80621421571f58d38b2325aa341f46ec79f2c7b
Kristoffer Dalby <kradalby@kradalby.no>
2014-06-20 14:17:19 +0200

    gave it feedback

--- ddns.py
+++ ddns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-""" Requierments
+""" 
+Requierments:
 requests
 """
 
@@ -10,12 +11,13 @@ SUBDOMAIN  = "tw"
 
 CLIENTID = ""
 APIKEY = ""
-APIURL = "https://api.digitalocean.com/domains/"
+APIURL = "https://api.digitalocean.com/v1/domains/"
 APIURL2 = "client_id=%s&api_key=%s" % (CLIENTID, APIKEY)
 
 def get_public_ip():
     r = requests.get("http://canihazip.com/s")
     return r.text
+
 
 def get_record_id(domain, subdomain=None):
     url = "%s/records?%s" % (domain, APIURL2)
@@ -30,6 +32,13 @@ def get_record_id(domain, subdomain=None):
     else:
         return False
 
+
+def get_current_dns_ip(domain, subdomain=None):
+    url = "%s/records/%s?%s" % (domain, get_record_id(domain, subdomain=subdomain), APIURL2)
+    r = requests.get(APIURL + url)
+    return r.json()['record']['data']
+
+
 def update_record(domain, ip, subdomain=None):
     url = "%s/records/%s/edit?%s&data=%s" % (domain, get_record_id(domain, subdomain=subdomain), APIURL2, ip)
     r = requests.get(APIURL + url)
@@ -39,8 +48,18 @@ def update_record(domain, ip, subdomain=None):
         return False
      
 
-update_record(DOMAIN, get_public_ip(), subdomain=SUBDOMAIN)
-                
+if __name__ == "__main__":
+    pub_ip = get_public_ip()
+    dns_ip = get_current_dns_ip(DOMAIN, subdomain=SUBDOMAIN)
+
+    if pub_ip == dns_ip:
+        print("Current ip: %s, no change, so no update" % pub_ip)
+    else: 
+        print("IP has changed from %s to %s, updating" % (dns_ip, pub_ip))
+        if update_record(DOMAIN, pub_ip, subdomain=SUBDOMAIN):
+            print("Update successful")
+        else:
+            print("Update failed")
                 
     
 
